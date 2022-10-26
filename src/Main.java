7b36288e4276b16da071dace91247dce121c9f2e
IliescuM <mihnea.iliescu06@gmail.com>
2022-10-27 00:00:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Account.OptionMenu;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        OptionMenu optionMenu = new OptionMenu();
+        optionMenu.getLogin();
+    }
+}
